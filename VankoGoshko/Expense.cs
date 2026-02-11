3e09c22a5e00e29cdff8aa1b00202787b7ab5f01
VankoPi4a <ia398031@gmail.com>
2026-02-11 10:26:09 +0200

    FIrst commit

---
+++ VankoGoshko/Expense.cs
@@ -0,0 +1,15 @@
+﻿// Expense.cs
+using System;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public string Category { get; set; }
+    public DateTime Date { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id}. {Amount:F2} лв | {Category} | {Date:yyyy-MM-dd}";
+    }
+}
---
+++ VankoGoshko/ExpenseManager.cs
@@ -0,0 +1,37 @@
+﻿// ExpenseManager.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ExpenseManager
+{
+    private List<Expense> expenses = new List<Expense>();
+    private int nextId = 1;
+
+    public void AddExpense(decimal amount, string category, DateTime date)
+    {
+        expenses.Add(new Expense
+        {
+            Id = nextId++,
+            Amount = amount,
+            Category = category,
+            Date = date
+        });
+    }
+
+    public List<Expense> GetAll()
+    {
+        return expenses;
+    }
+
+    public bool DeleteExpense(int id)
+    {
+        var expense = expenses.FirstOrDefault(e => e.Id == id);
+        if (expense != null)
+        {
+            expenses.Remove(expense);
+            return true;
+        }
+        return false;
+    }
+}
